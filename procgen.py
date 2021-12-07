8b80c01471f34bb489595ba9b6532e5c4816d7b9
Mauricio <mauricio.vittorazzi@itixti.com.br>
2021-12-07 19:40:21 -0300

    Dungeon procgen

---
+++ procgen.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+import random
+from typing import Iterator, List, Tuple, TYPE_CHECKING
+
+import tcod
+
+from game_map import GameMap
+import tile_types
+
+if TYPE_CHECKING:
+    from entity import Entity
+
+class RectangularRoom:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y + height
+
+    @property
+    def center(self) -> Tuple[int, int]:
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+
+        return center_x, center_y
+
+    @property
+    def inner(self) -> Tuple[slice, slice]:
+        # Retorna a área interior da sala como o índice de um array 2D.
+        return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+    
+    def intersects(self, other: RectangularRoom) -> bool:
+        # Retorna True se esta sala está sobrepondo outra RectangularRoom.
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
+
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+    # Retorna um tunel em formato de L entre os dois pontos.
+    x1, y1 = start
+    x2, y2 = end
+    if random.random() < 0.5: # Chance de 50%.
+        # Se move horizontalmente, depois, verticalmente.
+        corner_x, corner_y = x2, y1
+    else:
+        # Se move verticalmente, depois, horizontalmente.
+        corner_x, corner_y = x1, y2
+    
+    # Gera as coordenadas para o tunel.
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
+
+def generate_dungeon(
+    max_rooms: int,
+    room_min_size: int,
+    room_max_size: int,
+    map_width: int,
+    map_height: int,
+    player: Entity,
+) -> GameMap:
+    # Gera um novo mapa.
+    dungeon = GameMap(map_width, map_height)
+
+    rooms: List[RectangularRoom] = []
+
+    for r in range(max_rooms):
+        room_width = random.randint(room_min_size, room_max_size)
+        room_height = random.randint(room_min_size, room_max_size)
+
+        x = random.randint(0, dungeon.width - room_width - 1)
+        y = random.randint(0, dungeon.height - room_height - 1)
+
+        # A classe "RectangularRoom" deixa o trabalho com retângulos mais fácil.
+        new_room = RectangularRoom(x, y, room_width, room_height)
+
+        # Verifica se as salas estão sobrepostas.
+        if any(new_room.intersects(other_room) for other_room in rooms):
+            continue # Se a sala atual está sobreposta, vai para a próxima.
+        # Se não está sobreposta, a sala é válida.
+
+        # Escava a área interna da sala.
+        dungeon.tiles[new_room.inner] = tile_types.floor
+
+        if len(rooms) == 0:
+            # A primeira sala, onde o jogador começa.
+            player.x, player.y = new_room.center
+        else: # Todas as salas depois da primeira.
+            # Escava um tunel entre esta sala e a anterior.
+            for x, y in tunnel_between(rooms[-1].center, new_room.center):
+                dungeon.tiles[x, y] = tile_types.floor
+
+        # Enfim, acrescenta a sala à lista.
+        rooms.append(new_room)
+
+    return dungeon
