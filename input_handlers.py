dde6e760a6991a4f2cbdb98b9049d4bfba7389ae
Mauricio <mauricio.vittorazzi@itixti.com.br>
2021-12-13 20:54:18 -0300

    User Interface

--- input_handlers.py
+++ input_handlers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Optional, TYPE_CHECKING
 
-import tcod.event
+import tcod
 
 from actions import Action, BumpAction, EscapeAction, WaitAction
 
@@ -49,15 +49,26 @@ class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
         self.engine = engine
 
-    def handle_events(self) -> None:
-        raise NotImplementedError()
+    def handle_events(self, context: tcod.context.Context) -> None:
+        for event in tcod.event.wait():
+            context.convert_event(event)
+            self.dispatch(event)
+
+    def ev_mousemotion(self, event: tcod.event.MouseMotion) -> None:
+        if self.engine.game_map.in_bounds(event.tile.x, event.tile.y):
+            self.engine.mouse_location = event.tile.x, event.tile.y
 
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
 
+    def on_render(self, console: tcod.Console) -> None:
+        self.engine.render(console)
+
 class MainGameEventHandler(EventHandler):
-    def handle_events(self) -> None:
+    def handle_events(self, context: tcod.context.Context) -> None:
         for event in tcod.event.wait():
+            context.convert_event(event)
+
             action = self.dispatch(event)
 
             if action is None:
@@ -66,6 +77,7 @@ class MainGameEventHandler(EventHandler):
             action.perform()
 
             self.engine.handle_enemy_turns()
+
             self.engine.update_fov() # Atualiza o campo de visão antes da próxima ação do jogador.
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
@@ -83,11 +95,13 @@ class MainGameEventHandler(EventHandler):
         
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
+        elif key == tcod.event.K_v:
+            self.engine.event_handler = HistoryViewer(self.engine)
         
         return action
 
 class GameOverEventHandler(EventHandler):
-    def handle_events(self) -> None:
+    def handle_events(self, context: tcod.context.Context) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
 
@@ -105,3 +119,60 @@ class GameOverEventHandler(EventHandler):
             action = EscapeAction(self.engine.player)
 
         return action
+
+CURSOR_Y_KEYS = {
+    tcod.event.K_UP: -1,
+    tcod.event.K_DOWN: 1,
+    tcod.event.K_PAGEUP: -10,
+    tcod.event.K_PAGEDOWN: 10,
+}
+
+class HistoryViewer(EventHandler):
+    # Imprime um histórico em uma janela maior que pode ser navegável.
+
+    def __init__(self, engine: Engine):
+        super().__init__(engine)
+        self.log_length = len(engine.message_log.messages)
+        self.cursor = self.log_length - 1
+    
+    def on_render(self, console: tcod.Console) -> None:
+        super().on_render(console) # Desenha o estado principal como fundo.
+
+        log_console = tcod.Console(console.width - 6, console.height -6)
+        
+        # Desenha uma borda com uma etiqueta personagelizada.
+        log_console.draw_frame(0, 0, log_console.width, log_console.height)
+        log_console.print_box(
+            0, 0, log_console.width, 1, " Message history ", alignment=tcod.CENTER
+        )
+
+        # Desenha o log de mensagem usando parâmetro de cursor.
+        self.engine.message_log.render_messages(
+            log_console,
+            1,
+            1,
+            log_console.width - 2,
+            log_console.height - 2,
+            self.engine.message_log.messages[: self.cursor + 1],
+        )
+        log_console.blit(console, 3, 3)
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> None:
+        # Movimento condicional pra ficar bacana. :)
+        if event.sym in CURSOR_Y_KEYS:
+            adjust = CURSOR_Y_KEYS[event.sym]
+            if adjust < 0 and self.cursor == 0:
+                # Move a tela do topo para a parte inferior quando está nas bordas.
+                self.cursor = self.log_length - 1
+            elif adjust > 0 and self.cursor == self.log_length - 1:
+                # Idem para o movimento reverso.
+                self.cursor = 0
+            else:
+                # Caso contrário, move a janela enquanto presa aos limites do log de mensagens.
+                self.cursor = max(0, min(self.cursor + adjust, self.log_length - 1))
+        elif event.sym == tcod.event.K_HOME:
+            self.cursor = 0 # Move diretamente para a primeira mensagem.
+        elif event.sym == tcod.event.K_END:
+            self.cursor = self.log_length - 1 # Move diretamente para a última mensagem.
+        else: # Qualquer outra tecla move para o estado inicial.
+            self.engine.event_handler = MainGameEventHandler(self.engine)
---
+++ message_log.py
@@ -0,0 +1,62 @@
+from typing import List, Reversible, Tuple
+import textwrap
+
+import tcod
+
+import color
+
+class Message:
+    def __init__(self, text: str, fg: Tuple[int, int, int]):
+        self.plain_text = text
+        self.fg = fg
+        self.count = 1
+
+    @property
+    def full_text(self) -> str:
+        # O texto completo da mensagem, incluindo um contador se necessário.
+        if self.count > 1:
+            return f"{self.plain_text} (x{self.count})"
+        return self.plain_text
+
+
+class MessageLog:
+    def __init__(self) -> None:
+        self.messages: List[Message] = []
+    
+    def add_message(
+        self, text: str, fg: Tuple[int, int, int] = color.white, *, stack: bool = True,
+    ) -> None:
+        # Adiciona uma mensagem ao log.
+        # "Text" é a mensagem de texto, "fg" é a cor.
+        # Se "stack" for True então a mensagem acumula com a anterior com texto igual.
+        if stack and self.messages and text == self.messages[-1].plain_text:
+            self.messages[-1].count += 1
+        else:
+            self.messages.append(Message(text, fg))
+    
+    def render(
+        self, console: tcod.Console, x: int, y: int, width: int, height: int,
+    ) -> None:
+        # Renderiza o log de mensagem em uma área determinada.
+        # "x, "y", "width", "height" são a área retangular onde será renderizado no "console".
+        self.render_messages(console, x, y, width, height, self.messages)
+
+    @staticmethod
+    def render_messages(
+        console: tcod.console,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        messages: Reversible[Message],
+    ) -> None:
+        # Renderiza as mensagens recebidas.
+        # A variável "messages" é renderizada começando pela última mensagem.
+        y_offset = height - 1
+
+        for message in reversed(messages):
+            for line in reversed(textwrap.wrap(message.full_text, width)):
+                console.print(x=x, y=y + y_offset, string=line, fg=message.fg)
+                y_offset -= 1
+                if y_offset < 0:
+                    return # Sem espaço para eixibir mais mensagens.
